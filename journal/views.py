79a60f4c7a1239be1a0770003b7c1810d213dae1
FatimahMohamed <fatimah1pm@gmail.com>
2025-07-25 09:29:02 +0100

    clean up code

--- journal/views.py
+++ journal/views.py
@@ -3,10 +3,6 @@ from django.http import HttpResponse
 
 
 def home(request):
-    """
-    Home page view for the gratitude journal application.
-    This will display a welcome message for now.
-    """
     return HttpResponse(
         "<h1>Welcome to Gratitude Journal!</h1>"
         "<p>Your personal space for daily gratitude entries.</p>"
